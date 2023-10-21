9240098eea2a61cad247cb8a6fd370ed8bf6e890
terkage-backend <148375281+terkage-backend@users.noreply.github.com>
2023-10-21 19:43:47 +0100

    add business_logic directory, auth_logic file and login function from backend API

---
+++ src/business_logic/auth-logic.js
@@ -0,0 +1,32 @@
+const apiUrl = "http://127.0.0.1:8000/api";
+
+const login = async (email, password) => {
+    try {
+        const response = await fetch(`${apiUrl}/login/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+
+        if (!response.ok) {
+            // Handle error scenarios
+            console.error('A Server Error occurred', response);
+            return null;
+        }
+
+        const data = await response.json();
+        // Do something with the response data
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error("Error:", error);
+        return null;
+    }
+};
+
+
+export {
+    login
+}
